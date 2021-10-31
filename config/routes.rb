2dc6c8512761312e686182d33fc190c56b7959d7
iwayuka <iwabuchi.yuka07@gmail.com>
2021-10-31 00:51:53 +0000

    [Add] Controlloer Homes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # ユーザー認証機能のルーティング設定
   devise_for :users
+
+  # トップのURLを「https://〜amazonaws.com/」で表示されるようにする（ルート設定）
+  root to: 'homes#top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
