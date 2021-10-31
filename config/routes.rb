1675b517d09e2a5c863ffd4bb80007fd7a0bc0bb
iwayuka <iwabuchi.yuka07@gmail.com>
2021-10-31 01:23:45 +0000

    [Add] Controller Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   # ユーザー認証機能のルーティング設定
   devise_for :users
 
   # トップのURLを「https://〜amazonaws.com/」で表示されるようにする（ルート設定）
   root to: 'homes#top'
+
+  # Bookのルーティング設定
+  resources :books
+
+  # Userのルーティング設定
+  resources :users, only: [:index, :show, :edit]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
