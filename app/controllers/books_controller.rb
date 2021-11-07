7a6f7b1dabe6716096dd1d2017e3a6026f6aac8a
iwayuka <iwabuchi.yuka07@gmail.com>
2021-11-07 04:34:11 +0000

    [Add] Book編集のバリテーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,14 +11,21 @@ class BooksController < ApplicationController
 
 
   def create
-    # １. データを新規登録するためのインスタンス作成
+    # データを新規登録するためのインスタンス作成
     @book = Book.new(book_params)
+    # 投稿ユーザーidにはカレントユーザーidを自動的に保存する
     @book.user_id = current_user.id
-    # ２. データをデータベースに保存するためのsaveメソッド実行
-    @book.save
-    # ３. トップ画面へリダイレクト
-    redirect_to books_path
-
+    # データをデータベースに保存するためのsaveメソッド実行
+    # かつ、バリテーション の実行
+    if @book.save
+      # トップ画面へリダイレクトから投稿した詳細画面へリダイレクトに記述の変更
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      # データベースの保存の実行ができなかった場合、「今の画面」へ戻る
+      @books = Book.all
+      @user = User.find(current_user.id)
+      render :index
+    end
   end
 
   def show
@@ -31,19 +38,23 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
     # URLを直接入力しても、投稿ユーザーとカレントユーザーが同じで無ければ、編集できないようにする記述
-    if @book.user == current_user 
+    if @book.user == current_user
       render "edit"
     else
       redirect_to books_path
     end
-    # 
+    # 以上
 
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    # バリテーション の実行
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,9 @@ class Book < ApplicationRecord
   #association => User
   belongs_to :user
 
+  # バリテーション の記述（BOOKないでは「title」「body」に付与）
+  validates :title, presence: true
+  validates :body, presence: true
+  # 以上
+
 end
