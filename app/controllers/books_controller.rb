428be6b5ad3d90894764bc740055d3c4fba24a2c
iwayuka <iwabuchi.yuka07@gmail.com>
2021-11-08 12:07:20 +0000

    [Fix] 詳細画面と部分テンプレート内の投稿画面の両立

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,7 @@ class BooksController < ApplicationController
     @book = Book.new
     @books = Book.all
     @user = User.find(current_user.id)
-
   end
-
 
   def create
     # データを新規登録するためのインスタンス作成
@@ -31,6 +29,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = User.find(@book.user_id)
+    @booknew = Book.new
 
   end
 
